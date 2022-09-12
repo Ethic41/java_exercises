0c7aabfc4984927d416272f827a9d3742d586953
Dahir Muhammad Dahir <dahirmuhammad3@gmail.com>
2022-09-12 10:51:15 +0100

    update: completed two-fer

---
+++ two-fer/src/main/java/Twofer.java
@@ -0,0 +1,10 @@
+public class Twofer {
+    public String twofer(String name) {
+        if (name != "" && name != null){
+            return String.format("One for %s, one for me.", name);
+        }
+        else {
+            return "One for you, one for me.";
+        }
+    }
+}
